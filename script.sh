e49e9d64c6b2c92329de6285c4e85ca098010eeb
Ubuntu <ubuntu@ip-172-31-2-94.us-west-2.compute.internal>
2026-03-25 18:26:00 +0000

    adding helm install to install script

--- script.sh
+++ script.sh
@@ -22,4 +22,10 @@ sudo ./aws/install
 sudo apt update
 sudo apt install docker.io -y
 
+# Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh
+./get_helm.sh
+rm -f get_helm.sh
+
 echo "Installation completed successfully."
